a00c4f46d93c424bded97a4bc6fe14eff1bf93d6
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-03-27 14:10:45 +0300

    replace page.tsx with page.js for Dashboard component and update welcome message

---
+++ my-website/src/app/page.js
@@ -0,0 +1,12 @@
+export default function Dashboard() {
+  return (
+    <div className="p-5 text-black min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-3xl md:text-4xl sm:text-xl font-bold text-center">
+        Welcome to WhatBytes Dashboard
+      </h1>
+      <p className="mt-10 sm:mt-5 text-lg sm:text-base text-center font-bold text-blue-600">
+        This is WhatBytes Assignment for evaluation.
+      </p>
+    </div>
+  );
+}
